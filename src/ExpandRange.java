780fbf0522602515667eadc284936f57dbe57e32
Nasibulin <k.nasibulin@yandex.ru>
2018-09-17 16:42:07 +0300

    -

---
+++ src/ExpandRange.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 17.09.18
+ * Time: 16:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExpandRange {
+}
