f37072e7e038424c35dd1eb5cc3a3d897c661741
Nasibulin <k.nasibulin@yandex.ru>
2018-09-17 22:12:46 +0300

    -

---
+++ src/Regexp.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 17.09.18
+ * Time: 20:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Regexp {
+}
---
+++ src/Replacement.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 17.09.18
+ * Time: 21:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Replacement {
+}
