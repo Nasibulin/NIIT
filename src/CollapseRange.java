f96029ceb6b91d34b62d89112a09a652487d2b3f
Nasibulin <k.nasibulin@yandex.ru>
2018-09-18 16:45:53 +0300

    -

---
+++ src/CollapseRange.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 18.09.18
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class CollapseRange {
+}
