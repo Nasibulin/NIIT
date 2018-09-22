0876660c4386060c221bb4951460262e9030f5cb
Nasibulin <k.nasibulin@yandex.ru>
2018-09-22 07:30:32 +0300

    -

--- src/ReadBytesFromFile.java
+++ src/ReadBytesFromFile.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Konstantin
@@ -5,5 +7,21 @@
  * Time: 7:27
  * To change this template use File | Settings | File Templates.
  */
-public class ReadBytesFromFile {
+class ReadBytesFromFile {
+    public static void main(String args[]) throws Exception {
+        // getBytes from anyWhere
+        // I'm getting byte array from File
+        File file = null;
+        FileInputStream fileStream = new FileInputStream(file = new File("C:\\Users\\Konstantin\\IdeaProjects\\NIIT\\src\\0"));
+
+        // Instantiate array
+        byte[] arr = new byte[(int) file.length()];
+
+        // read All bytes of File stream
+        fileStream.read(arr, 0, arr.length);
+
+        for (int X : arr) {
+            System.out.print((char) X);
+        }
+    }
 }
