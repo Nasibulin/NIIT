41faa19b82d5ad6b3d1307cd120f682b23121364
Nasibulin <k.nasibulin@yandex.ru>
2018-09-22 07:30:05 +0300

    -

---
+++ src/ReadBytesFromFile.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 22.09.18
+ * Time: 7:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReadBytesFromFile {
+}
