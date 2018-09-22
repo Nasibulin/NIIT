3ee82a1e9912d4861954aa5ec349edbfa90601fe
Nasibulin <k.nasibulin@yandex.ru>
2018-09-22 07:55:10 +0300

    -

--- src/ReadBytesFromFile.java
+++ src/ReadBytesFromFile.java
@@ -11,17 +11,20 @@ class ReadBytesFromFile {
     public static void main(String args[]) throws Exception {
         // getBytes from anyWhere
         // I'm getting byte array from File
+        for (int i=0;i<10;i++) {
         File file = null;
-        FileInputStream fileStream = new FileInputStream(file = new File("C:\\Users\\Konstantin\\IdeaProjects\\NIIT\\src\\0"));
+        FileInputStream fileStream = new FileInputStream(file = new File("C:\\Users\\Konstantin\\IdeaProjects\\NIIT\\src\\"+i));
 
         // Instantiate array
-        byte[] arr = new byte[(int) file.length()];
+        byte[][] arr = new byte[10][(int) file.length()];
 
         // read All bytes of File stream
-        fileStream.read(arr, 0, arr.length);
+        fileStream.read(arr[i], 0, arr.length);
 
-        for (int X : arr) {
+        for (int X : arr[i]) {
             System.out.print((char) X);
+        }
+            System.out.println("");
         }
     }
 }
