c22763308da5d4d2788bb0ff7dc71aa6a0cdd761
Nasibulin <k.nasibulin@yandex.ru>
2018-09-23 08:04:37 +0300

    -

---
+++ src/Circle.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 23.09.18
+ * Time: 7:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Circle {
+}
