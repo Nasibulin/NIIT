3f7e5ec645848d9d5011eef340d200ce7705460c
Nasibulin <k.nasibulin@yandex.ru>
2018-09-25 19:53:14 +0300

    -

--- src/Automata.java
+++ src/Automata.java
@@ -6,14 +6,62 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Automata {
-    int cash;
-    String[] menu;
-    int prices;
-    STATES state;
+    private int cash;
+    private String[] menu;
+    private int[] prices;
+    private STATES state;
 
-    public Automata() {
+    public Automata(String[] menu, int[] prices) {
+
+        this.menu = menu;
+        this.prices = prices;
+        this.state = STATES.OFF;
 
     }
 
+    public static void main(String[] args) {
+        Automata a = new Automata(new String[]{"Coca-cola", "Tea", "Coffee"}, new int[]{12, 10, 18});
+
+    }
+
+    public void on() {
+        state = STATES.WAIT;
+    }
+
+    public void off() {
+        state = STATES.OFF;
+    }
+
+    public void coin() {
+
+    }
+
+    public void printMenu() {
+
+    }
+
+    public void printState() {
+
+    }
+
+    public void choice() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void cancel() {
+
+    }
+
+    public void cook() {
+
+    }
+
+    public void finish() {
+
+    }
 
 }
