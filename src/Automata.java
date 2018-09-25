2cd9db1937101f7af50c32ac8fba6282d37bc175
Nasibulin <k.nasibulin@yandex.ru>
2018-09-25 14:22:32 +0300

    -

---
+++ src/Automata.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 25.09.18
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Automata {
+}
---
+++ src/STATES.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 25.09.18
+ * Time: 14:20
+ * To change this template use File | Settings | File Templates.
+ */
+public enum STATES {
+}
