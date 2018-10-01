9e5079aa492bd35ad477a804a1bf5dac54356a4d
Nasibulin <k.nasibulin@yandex.ru>
2018-10-01 12:49:44 +0300

    -

---
+++ src/javatemp/AutomataDemo.java
@@ -0,0 +1,11 @@
+package javatemp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 01.10.18
+ * Time: 11:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class AutomataDemo {
+}
