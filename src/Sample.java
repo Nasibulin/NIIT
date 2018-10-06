95f518ef440b38c97d5ec327dd02403ee859f87b
Nasibulin <k.nasibulin@yandex.ru>
2018-10-06 11:54:52 +0300

    -

---
+++ src/Sample.java
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 29.09.18
+ * Time: 20:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sample {
+    static int x;
+    String s;
+
+    public Sample(String s) {
+     this.s=s;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(x);
+    }
+
+
+}
