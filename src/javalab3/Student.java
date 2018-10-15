36e6c8bb0d3e5fedbf8ae2a15a65ab646bb84660
Nasibulin <k.nasibulin@yandex.ru>
2018-10-15 10:54:49 +0300

    -

---
+++ src/javalab3/Student.java
@@ -0,0 +1,11 @@
+package javalab3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 15.10.18
+ * Time: 10:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Student {
+}
