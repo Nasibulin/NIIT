44d0acb5c9298710cea0ecfa86d3239a051a4cad
Nasibulin <k.nasibulin@yandex.ru>
2018-10-16 15:08:32 +0300

    -

---
+++ src/javalab3/DekanatDemo.java
@@ -0,0 +1,11 @@
+package javalab3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 16.10.18
+ * Time: 10:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class DekanatDemo {
+}
