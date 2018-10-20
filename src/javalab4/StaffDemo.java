edc0053d471a7a8295c3e2938384d939765e2b2b
Nasibulin <k.nasibulin@yandex.ru>
2018-10-20 14:09:32 +0300

    -

---
+++ src/javalab4/StaffDemo.java
@@ -0,0 +1,11 @@
+package javalab4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 20.10.18
+ * Time: 12:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class StaffDemo {
+}
