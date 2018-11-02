accea0eddb87308e014217a084d900dea3d5d8e5
Nasibulin <k.nasibulin@yandex.ru>
2018-11-02 14:43:32 +0300

    -

--- src/javalab6/ReadXMLUrl.java
+++ src/javalab6/ReadXMLUrl.java
@@ -8,15 +8,16 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.net.URL;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class ReadXMLUrl {
 
-    public static void main(String argv[]) {
-
+    public static void main(String args[]) {
+        Locale ru = Locale.getDefault();
         try {
 
             String url = "https://www.cbr.ru/scripts/XML_daily.asp";
-            //File fXmlFile = new File("/Users/mkyong/staff.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(new URL(url).openStream());
@@ -25,7 +26,9 @@ public class ReadXMLUrl {
             //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
             doc.getDocumentElement().normalize();
 
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName()+" "+doc.getDocumentElement().getAttribute("Date"));
+            System.out.println(
+                    "Root element :" + doc.getDocumentElement().getNodeName() + " " + doc.getDocumentElement().getAttribute(
+                            "Date"));
 
             NodeList nList = doc.getElementsByTagName("Valute");
 
@@ -42,11 +45,15 @@ public class ReadXMLUrl {
                     Element eElement = (Element) nNode;
 
                     System.out.println("Valute ID : " + eElement.getAttribute("ID"));
-                    System.out.println("NumCode : " + eElement.getElementsByTagName("NumCode").item(0).getTextContent());
-                    System.out.println("CharCode : " + eElement.getElementsByTagName("CharCode").item(0).getTextContent());
-                    System.out.println("Nominal : " + eElement.getElementsByTagName("Nominal").item(0).getTextContent());
+                    System.out.println(
+                            "NumCode : " + eElement.getElementsByTagName("NumCode").item(0).getTextContent());
+                    System.out.println(
+                            "CharCode : " + eElement.getElementsByTagName("CharCode").item(0).getTextContent());
+                    System.out.println(
+                            "Nominal : " + eElement.getElementsByTagName("Nominal").item(0).getTextContent());
                     System.out.println("Name : " + eElement.getElementsByTagName("Name").item(0).getTextContent());
-                    System.out.println("Value : " + eElement.getElementsByTagName("Value").item(0).getTextContent());
+                    System.out.println("Value : " + NumberFormat.getInstance(Locale.FRANCE).parse(
+                            eElement.getElementsByTagName("Value").item(0).getTextContent()).doubleValue());
 
                 }
             }
