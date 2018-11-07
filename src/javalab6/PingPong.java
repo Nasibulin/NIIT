970baedcb4bd334f264fa52f0332ee6c8ffd4058
Nasibulin <k.nasibulin@yandex.ru>
2018-11-07 15:54:03 +0300

    -

---
+++ src/javalab6/PingPong.java
@@ -0,0 +1,11 @@
+package javalab6;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 06.11.18
+ * Time: 16:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class PingPong {
+}
---
+++ src/javatemp/TestASClient.java
@@ -0,0 +1,11 @@
+package javatemp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nasibulin
+ * Date: 07.11.18
+ * Time: 8:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestASClient {
+}
