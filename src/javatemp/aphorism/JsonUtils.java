31870553a883132fef1ebca046b89ee125e263c9
Nasibulin <k.nasibulin@yandex.ru>
2018-11-08 21:36:20 +0300

    -

---
+++ src/javatemp/aphorism/JsonUtils.java
@@ -0,0 +1,11 @@
+package javatemp.aphorism;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 08.11.18
+ * Time: 20:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class JsonUtils {
+}
---
+++ src/javatemp/aphorism/Quote.java
@@ -0,0 +1,11 @@
+package javatemp.aphorism;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Konstantin
+ * Date: 08.11.18
+ * Time: 21:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Quote {
+}
