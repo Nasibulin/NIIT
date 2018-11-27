4fb3922ef0037b7267df6042a3fa060f344ed801
Nasibulin <k.nasibulin@yandex.ru>
2018-11-27 07:18:59 +0300

    -

---
+++ src/hashexmpl/HashMapStructure.java
@@ -0,0 +1,35 @@
+package hashexmpl;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class HashMapStructure {
+
+    /**
+     * @author Arpit Mandliya
+     */
+    public static void main(String[] args) {
+
+        Country india=new Country("India",1000);
+        Country japan=new Country("Japan",10000);
+
+        Country france=new Country("France",2000);
+        Country russia=new Country("Russia",20000);
+
+        HashMap<Country, String> countryCapitalMap=new HashMap<Country,String>();
+        countryCapitalMap.put(india,"Delhi");
+        countryCapitalMap.put(japan,"Tokyo");
+        countryCapitalMap.put(russia,"Moscow");
+        countryCapitalMap.put(france,"Paris");
+
+        Iterator<Country> countryCapitalIter=countryCapitalMap.keySet().iterator();//put debug point at this line
+        while(countryCapitalIter.hasNext())
+        {
+            Country countryObj=countryCapitalIter.next();
+            String capital=countryCapitalMap.get(countryObj);
+            System.out.println(countryObj.getName()+"----"+capital);
+        }
+    }
+
+
+}
